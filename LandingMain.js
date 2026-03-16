dfea4ea0c4315b5d659c3c9de0a28c3c2d3a89bc
amira_nasser <amira20nasser@gmail.com>
2026-03-16 03:31:01 +0200

    Add ProjectsSection component and integrate it into the main application

--- LandingMain.js
+++ LandingMain.js
@@ -2,12 +2,14 @@ import { NavBar } from './nav.js';
 import { HeroSection } from './Hero.js';
 import { Footer } from './Footer.js';
 import { WorkSection } from './WorkSection.js';
+import { ProjectsSection } from './ProjectsSection.js';
 const App = () => {
   const root = document.getElementById('app');
   root.innerHTML = `
     ${NavBar()}
     ${HeroSection()}
     ${WorkSection()}
+    ${ProjectsSection()}
     ${Footer()}
   `;
 };
