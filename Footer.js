006ed5bce05579e3c2ea9cee5929642aeb90ea5f
amira_nasser <amira20nasser@gmail.com>
2026-03-16 03:15:31 +0200

    Add Footer component and integrate it into the main application

---
+++ Footer.js
@@ -0,0 +1,28 @@
+export const Footer = () => {
+    return `
+    <footer class="container my-4 footer">
+        <div class="row">
+            <div class="col-md-6">
+                <a class="navbar-brand fs-3 text-white fw-bold" href="#">Finsweet</a>
+                <p class="fw-light custom-fs mt-2 w-75">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
+                </p>
+            </div>
+            <div class="col-md-6 mt-4 mt-md-0">
+                <p class=" fs-1 text-white fw-bold">Let's Talk!</p>
+                <p class="fw-light custom-fs mt-2 w-75">
+                    We are always open to discuss your project, improve your online presence and help with your UX/UI
+                    design challenges.
+                </p>
+                <div class="d-flex align-items-center gap-4">
+                    <i class="fa-brands fa-facebook"></i>
+                    <i class="fa-brands fa-twitter"></i>
+                    <i class="fa-brands fa-linkedin"></i>
+                    <i class="fa-brands fa-instagram"></i>
+                </div>
+
+            </div>
+
+        </div>
+    </footer>`;
+}
--- LandingMain.js
+++ LandingMain.js
@@ -1,10 +1,12 @@
 import { NavBar } from './nav.js';
 import { HeroSection } from './Hero.js';
+import { Footer } from './Footer.js'
 const App = () => {
-    const root = document.getElementById('app');
-    root.innerHTML = `
+  const root = document.getElementById('app');
+  root.innerHTML = `
     ${NavBar()}
     ${HeroSection()}
+    ${Footer()}
   `;
 };
 App()
